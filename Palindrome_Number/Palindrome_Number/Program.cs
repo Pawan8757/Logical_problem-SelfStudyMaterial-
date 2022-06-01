664f3539375d072ba4a7d46bb6fe81ae4ae2cb08
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-06-01 07:21:17 +0530

    program for palindrome number

---
+++ Palindrome_Number/Palindrome_Number/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace number
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a valid input");
+            int num=Convert.ToInt32(Console.ReadLine());
+
+            int rev = 0;
+            int rem = 0;
+            int backup = num;
+            while(num > 0)
+            {
+                rem = num % 10;
+                rev = rev * 10 + rem;
+                num= num / 10;
+            }
+            if(backup==rev)
+            {
+                Console.WriteLine("It's palindrome");
+            }
+            else
+            {
+                Console.WriteLine("It's not a palindrome number");
+            }
+        }
+    }
+}
