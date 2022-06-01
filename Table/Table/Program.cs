57a71c5e5b4800342e73d49d36da594e69fae6b5
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-06-01 07:28:06 +0530

    Multiplication table , created

---
+++ Table/Table/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Easy
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number whose multiplication you want");
+            int num=Convert.ToInt32(Console.ReadLine());
+
+            int cube = num * num * num;
+            Console.WriteLine(cube);
+        }
+    }
+}
