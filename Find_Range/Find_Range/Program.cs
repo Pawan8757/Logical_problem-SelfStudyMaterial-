1f1b01116ebe76ef7c0463dcf60bdd4070c0cdfa
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-06-01 07:17:37 +0530

    calculating range for a number

---
+++ Find_Range/Find_Range/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Easy
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter starting value");
+            int i = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Ending value");
+            int j = Convert.ToInt32(Console.ReadLine());
+            
+            for (int num =i; num <=j; num++)
+            {
+                int count = 0;
+
+                for (int k=1;k<=num;k++)
+                    {
+                        if(num%k==0)
+                        {
+                            count++;
+                        }
+                    }
+                    if(count==2)
+                    {
+                        Console.WriteLine(fac);
+                    }
+                    
+                }
+            Console.ReadLine();
+        }
+        }
+    }
