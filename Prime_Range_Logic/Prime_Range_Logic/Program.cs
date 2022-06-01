42c277987bec66cde1229b7301da6eb8bccf648b
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-06-01 07:23:10 +0530

    calculate prime range logic

---
+++ Prime_Range_Logic/Prime_Range_Logic/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Easy
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter starting range");
+            int i=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter ending range");
+            int j=Convert.ToInt32(Console.ReadLine());
+
+            for(int num=i; num<=j; num++)
+            {
+                int count = 0;
+                for(int k=1; k<=num; k++)
+                {
+                    if (num % k == 0)
+                    {
+                        count++;
+                    }
+                   
+                }
+                if(count == 2)
+                {
+                    Console.WriteLine(num);
+                }
+               
+            }
+            Console.ReadLine();
+        }
+    }
+}
