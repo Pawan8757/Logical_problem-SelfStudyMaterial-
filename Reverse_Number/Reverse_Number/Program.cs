2db3f295ee25387008244682ba5af8b360eae1b7
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-06-01 07:23:35 +0530

    Program for Reverse number

---
+++ Reverse_Number/Reverse_Number/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace number
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a valid input");
+            int num=Convert.ToInt32(Console.ReadLine());
+
+            int backup = num;
+            int rem, rev = 0;
+            while(num>0)
+            {
+                rem = num % 10;
+                rev = rev * 10 + rem;
+                num = num / 10;
+
+            }
+            Console.WriteLine($"Reverse number is {rev}");
+        }
+    }
+}
