6c95300e8430cf8e106dd45fe2e971ab5438a43e
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-06-01 07:15:37 +0530

    logic for fibonaccis series

---
+++ Fibanociii_Logic/Fibanociii_Logic/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Easy
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            int a = 0,b = 1, c;
+            Console.WriteLine(a + " " + b);
+            Console.WriteLine("Enter a valid input");
+            int num=Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 2; i <=num; i++)
+            {
+               c = a + b;
+               Console.WriteLine(c + " ");
+               a = b;
+               b = c;
+            }
+            
+        }
+    }
+}
