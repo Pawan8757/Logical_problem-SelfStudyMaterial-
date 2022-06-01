ca4467faa4ba4925cac13e332bfa31a95986c292
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-06-01 07:14:41 +0530

    Logic for armstrong number

---
+++ ArmStrong_Number/ArmStrong_Number/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace number
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a valid input");
+            int num=Convert.ToInt32(Console.ReadLine());
+
+            int result = 0;
+            int temp = num;
+            int rem = 0;
+            while(temp!=0)
+            {
+                rem = temp % 10;
+                result += rem * rem * rem;
+                temp = temp / 10;
+            }
+            if(num==result)
+            {
+                Console.WriteLine("{0} is a armstrong number",num);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not an armstrong number", num);
+            }
+        }
+    }
+}
