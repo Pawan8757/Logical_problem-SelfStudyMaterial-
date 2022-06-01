c6a3cc011832f8e01c0363fef4f34a7f8138f9a1
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-06-01 07:20:17 +0530

    Program for Leap Year

---
+++ Leap_Year/Leap_Year/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leap_Year
+{
+    public  class Class1
+    {
+        public void FindingLeap()
+        {
+            Console.WriteLine("Enter the year");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if(num%4==0 && num%100!=0 || num%400==0)
+            {
+                Console.WriteLine("the given year {num} is a leap year");
+            }
+            else
+            {
+                Console.WriteLine("NOT A LEAP YEAR");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Leap_Year/Leap_Year/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Leap_Year
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the c# tutorial");
+            Console.WriteLine("\n \n ");
+            Class1 class1 = new Class1();
+            class1.FindingLeap();
+        }
+    }
+}
