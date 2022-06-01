8cbfe38acaad030222e597f4f3546ec5f5466f71
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-06-01 07:30:26 +0530

    Converting farheinheit to celcius and vice-versa

---
+++ Temperature_Conversion/Temperature_Conversion/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Temperature_Conversion
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("this is the temperature conversion tutorial");
+            Temp temp = new Temp();
+            temp.FarToCel();
+            //temp.CelToFar();
+        }
+    }
+}
---
+++ Temperature_Conversion/Temperature_Conversion/Temp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Temperature_Conversion
+{
+    class Temp
+    {
+        public void CelToFar()
+        {
+            Console.WriteLine("Enter the celcius temperature");
+            int celcius=Convert.ToInt32(Console.ReadLine());
+
+            double farhenheit = (celcius * 9) / 5 + 32;
+            Console.WriteLine("Farheit would be "+farhenheit);
+
+        }
+
+        public void FarToCel()
+        {
+            Console.WriteLine("Enter the farheinheit temperature");
+            int farheinheit = Convert.ToInt32(Console.ReadLine());
+
+            double celcius = (farheinheit - 32) * 5 / 9;
+            Console.WriteLine("Celcius would be "+celcius);
+
+        }
+    }
+}
