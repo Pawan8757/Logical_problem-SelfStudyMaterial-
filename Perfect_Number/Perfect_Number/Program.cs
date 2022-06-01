f55cbebb35ad27ed7b45076c5d69eb121e74c025
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-06-01 07:21:52 +0530

    Programing logic for Perfect number

---
+++ Perfect_Number/Perfect_Number/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Perfect_Number;
+public class Exercise28
+{
+    public static void Main()
+    {
+
+
+        Console.Write("Find perfect numbers within a given number of range:\n");
+        Console.Write("\n\n");
+
+        Console.Write("Enter the number :");
+        int num=Convert.ToInt32(Console.ReadLine());
+
+        int i, sum = 0;
+        for(i = 1; i < num; i++)
+        {
+            if(num%i==0)
+            {
+                sum = sum + i;
+            }
+        }
+        if(sum==num)
+        {
+            Console.WriteLine("The {num} is a perfect number");
+        }
+        else
+        {
+            Console.WriteLine("NOT A PERFECT NUMBER");
+        }
+    }
+}
