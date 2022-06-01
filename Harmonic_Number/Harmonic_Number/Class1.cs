59f53451f609d20f6b699100dd21e883c7376bc2
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-06-01 07:19:21 +0530

    program for harmonic number

---
+++ Harmonic_Number/Harmonic_Number/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace number
+{
+    public class Findout
+    {
+        public void calcultingHarmonic()
+        {
+            Console.WriteLine("Enter the valid input");
+            int num= Convert.ToInt32(Console.ReadLine());
+          
+            double harmonic = 0.0;
+            for(int i = 1; i <= num; i++)
+            {
+                Console.WriteLine("1/{0}", i);
+                harmonic = harmonic + 1/(float)i;
+            }
+            Console.WriteLine("\n \n");
+
+            Console.WriteLine("the harmonic value of {num} are " + harmonic);
+        }
+    }
+}
---
+++ Harmonic_Number/Harmonic_Number/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace number
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to my practise agenda");
+            Findout findout = new Findout();
+            findout.calcultingHarmonic();
+        }
+    }
+}
