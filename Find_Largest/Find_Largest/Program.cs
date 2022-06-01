86b160c916ff2c1714c221e54ad56d8982b44ade
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-06-01 07:17:03 +0530

    check the largest number

---
+++ Find_Largest/Find_Largest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Easy
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter three number");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            if(a > b && a>c)
+            {
+                Console.WriteLine("A is the greatest number");
+            }
+            else if(b>a && b>c)
+            {
+                Console.WriteLine("B is the greatest number");
+            }
+            else
+            {
+                Console.WriteLine("C is the greatest one");
+            }
+            Console.ReadLine();
+        }
+    }
+}
