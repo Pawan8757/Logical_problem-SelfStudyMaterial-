83cef558e2d82e7c6efe5b553d31660bbbbf1455
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-06-01 07:15:03 +0530

    program for factorial

---
+++ Factorial/Factorial/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace factorial
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number whose factorial you want");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int fact = 1;
+            for(int i = 1; i <= num; i++)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine("the factorial of {0}! is {1}", num, fact);
+        }
+    }
+}
