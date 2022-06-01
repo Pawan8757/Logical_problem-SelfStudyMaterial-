58e48ef0376ee35d545b920c6a6aad66f399ffcc
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-06-01 07:24:00 +0530

    Logic for string number

---
+++ String_Number/String_Number/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Easy
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            string name = "MOM";
+            string rev = "";
+
+            for(int i =name.Length-1;i>=0;i--)
+            {
+                rev=rev+name[i];
+            }
+            
+           if(rev==name)
+            {
+                Console.WriteLine("given no is an armstrong number");
+            }
+            else
+            {
+                Console.WriteLine("Not an armstrong number");
+            }
+        }
+    }
+
+}
