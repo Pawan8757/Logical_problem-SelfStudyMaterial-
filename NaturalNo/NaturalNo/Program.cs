b5a57e876216617113623fe8210a06f84d63c17d
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-06-01 07:20:50 +0530

    adding natural number logic

---
+++ NaturalNo/NaturalNo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace SumOfNaturalNumber
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a valid input");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int sum = 0;
+            for (int i = 0; i <=num; i++)
+            {
+                if(i%2==0)
+                {
+                    sum = sum + i;  
+                }              
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
