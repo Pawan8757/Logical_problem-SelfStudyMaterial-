5852f78392df4f8606877c62cfcd98a26a594c03
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-06-01 07:22:34 +0530

    Programming logic for prime number

---
+++ Prime_number/Preparation/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace prime
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the valid prime number");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int m = num / 2;
+            int flag = 0;
+
+            for(int i=2;i<=m;i++)
+            {
+                if(num%i==0)
+                {
+                    Console.WriteLine("given no is not a prime number");
+                    flag = 1;
+                    break;
+                }
+                
+            }
+            if (flag == 0)
+            {
+                Console.WriteLine("its a prime number");
+            }
+
+        }
+
+    }
+}
