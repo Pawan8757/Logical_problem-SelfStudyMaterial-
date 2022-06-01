c398c7a55fc6fecdfc68332dfba66ca0599fe8e5
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-06-01 07:18:29 +0530

    To calculate factor, logic are written

---
+++ FindingFactor/FindingFactor/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Easy
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a valid input");
+            int i=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter a end valid input");
+            int j = Convert.ToInt32(Console.ReadLine());
+
+            for(int k=i; k<=j; k++)
+            {
+                int count = 0;
+                for (int num = 1; num <= k; num++)
+                {
+                    if (k % num == 0)
+                    {
+                        count++;
+                    }
+                }
+                if (count == 2)
+                {
+                    Console.WriteLine(k);
+                }
+            }
+        }
+    }
+}
